9bbe6fd6b5f27195b68bc9754128624dbe5240bc
Amina Rahman Nisha <aminarahman01@gmail.com>
2022-06-30 18:32:21 +0600

    Create exercise2.js

---
+++ exercise2.js
@@ -0,0 +1,18 @@
+let array = [1,2,3, 5];
+
+function findComponents(array, targetNumber) {
+	let ans = [];
+	for(let i = 0; i < array.length; i++) {
+		let temp = (targetNumber - array[i]) > 0 ? targetNumber - array[i] : array[i] - targetNumber;
+
+		if(array.find(num => num === temp)) {
+			ans = ans.concat(array[i], temp);
+			break;
+		}
+
+	}
+	if(ans.length == 0) return undefined;
+	return ans;
+}
+
+let ans = findComponents(array, 4);
